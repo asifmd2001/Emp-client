017b21f828de47adf35dd64a8141883ba6d8d002
asifmd2001 <asifmohamedsm2001@gmail.com>
2022-01-21 21:51:54 +0530

    1 file modified

--- src/api/index.js
+++ src/api/index.js
@@ -1,9 +1,15 @@
 import axios from 'axios';
 
 // const pu = 'https://cors-anywhere.herukuapp.com/';
+const config = {
+  headers: {
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Methods': 'GET,PUT,POST,DELETE,PATCH,OPTIONS',
+  },
+};
 
 const url = 'https://node-nhwtpw--5000.local.webcontainer.io/emp';
 
-export const fetchEmps = () => axios.get(url);
+export const fetchEmps = () => axios.get(url, config);
 
-export const createEmp = (newEmp) => axios.post(url, newEmp);
+export const createEmp = (newEmp) => axios.post(url, newEmp, config);
