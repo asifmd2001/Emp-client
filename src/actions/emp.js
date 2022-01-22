17bf0d7fdc7fe5a7e5b5862cda712e1520fd85f3
asifmd2001 <asifmohamedsm2001@gmail.com>
2022-01-22 11:22:44 +0530

    2 files modified

--- src/actions/emp.js
+++ src/actions/emp.js
@@ -1,22 +1,20 @@
-import * as api from  "../api";
+import * as api from '../api';
 
 export const getEmps = () => async (dispatch) => {
-    try {
-        const { data } = await api.fetchEmps();
-        const action = { type: "FETCH_ALL", payload: data };
-        dipatch(action);
-    }
-    catch (error) {
-        console.log(error);
-    }
+  try {
+    const { data } = await api.fetchEmps();
+    const action = { type: 'FETCH_ALL', payload: data };
+    dispatch(action);
+  } catch (error) {
+    console.log(error);
+  }
 };
-export const createEmp = (post) => async (dipatch) => {
-    try {
-        const { data } = await api.createEmp(post);
-        const action = { type: "CREATE", payload: data };
-        dipatch(action);
-    }
-    catch (error) {
-        console.log(error);
-    }
+export const createEmp = (post) => async (dispatch) => {
+  try {
+    const { data } = await api.createEmp(post);
+    const action = { type: 'CREATE', payload: data };
+    dispatch(action);
+  } catch (error) {
+    console.log(error);
+  }
 };
--- src/api/index.js
+++ src/api/index.js
@@ -8,9 +8,9 @@ const config = {
   },
 };
 
-// const url = 'https://node-nhwtpw--5000.local.webcontainer.io/emp';
-const url = "https://emp-backend.mohamed-asifasi.repl.co/emp";
+const url = 'https://node-nhwtpw--5000.local.webcontainer.io/emp';
+// const url = "https://emp-backend.mohamed-asifasi.repl.co/emp";
 
-export const fetchEmps = () => axios.get(url, config);
+export const fetchEmps = () => axios.get(url);
 
-export const createEmp = (newEmp) => axios.post(url, newEmp, config);
+export const createEmp = (newEmp) => axios.post(url, newEmp);
